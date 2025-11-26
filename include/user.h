2d8b48fd57afbfa9383ac407404fe3ae80f9ea68
Krishna <krishnapipraiya101@gmail.com>
2025-11-27 01:10:24 +0530

    Add files via upload

---
+++ include/user.h
@@ -0,0 +1,34 @@
+#ifndef USER_H
+#define USER_H
+
+struct User {
+    char name[50];
+    char contact[20];
+    char gender[10];
+    int age;
+    float height;
+    float weight;
+    char goal[20];
+    char activity[20];
+    int goalSteps;
+    float goalWeight;
+    float ratePerWeek;
+
+    float caloriesTaken;
+    int stepsToday;
+    float currentWeight;
+};
+
+// Function declarations
+void signupOrLogin();
+int userExists(char name[], char contact[], struct User *u);
+void saveUser(struct User u);
+float calculateBMR(struct User u);
+void mainMenu(struct User user);
+void calorieSection(float *caloriesTaken, float calorieNeed);
+void stepsSection(int *stepsToday, int goalSteps);
+void weightSection(struct User *u);
+void bmiSection(struct User u);
+void suggestionSection(struct User u, float calorieNeed, float caloriesTaken, int stepsToday);
+
+#endif
